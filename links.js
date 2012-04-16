d876462875c3a912d9d4755b7d022d33b18a2685
mksiazek <mksiazek@gmail.com>
2012-04-16 09:53:25 +0000

    added links to scene added simple stylesheet

---
+++ links.js
@@ -0,0 +1,65 @@
+﻿function TOP(viewer)
+{
+	viewer.scale = 0.25;
+	viewer.yaw = 0;
+	viewer.pitch = 90;
+	viewer.x = 0;
+	viewer.y = 0;
+	viewer.lastX = 0;
+	viewer.lastY = 0;
+}
+
+function BOTTOM(viewer)
+{
+	viewer.scale = 0.25;
+	viewer.yaw = 0;
+	viewer.pitch = -90;
+	viewer.x = 0;
+	viewer.y = 0;
+	viewer.lastX = 0;
+	viewer.lastY = 0;
+}
+
+function LEFT(viewer)
+{
+	viewer.scale = 0.25;
+	viewer.yaw = -90;
+	viewer.pitch = 0;
+	viewer.x = 0;
+	viewer.y = 1;
+	viewer.lastX = 0;
+	viewer.lastY = 0;
+}
+
+function RIGHT(viewer)
+{
+	viewer.scale = 0.25;
+	viewer.yaw = 90;
+	viewer.pitch = 0;
+	viewer.x = 0;
+	viewer.y = 1;
+	viewer.lastX = 0;
+	viewer.lastY = 0;
+}
+
+function FRONT(viewer)
+{
+	viewer.scale = 0.25;
+	viewer.yaw = 0;
+	viewer.pitch = 0;
+	viewer.x = 0;
+	viewer.y = 1;
+	viewer.lastX = 0;
+	viewer.lastY = 0;
+}
+
+function BACK(viewer)
+{
+	viewer.scale = 0.25;
+	viewer.yaw = 180;
+	viewer.pitch = 0;
+	viewer.x = 0;
+	viewer.y = 0;
+	viewer.lastX = 0;
+	viewer.lastY = 0;
+}

7a570b5269411103e52ab1f83056618ac9db132b
mksiazek@gmail.com <mksiazek@gmail.com@9398bd94-36a7-92ba-80c6-c49b0c37730a>
2012-04-16 17:22:58 +0000

    added custom deep links with auto update

--- trunk/links.js
+++ trunk/links.js
@@ -1,4 +1,41 @@
-﻿function TOP(viewer)
+﻿var separator = "|";
+var paramCount = 7;
+
+function SetAddress()
+{
+	var link = "";
+	link += viewer.scale.toFixed(2)+separator;
+	link += viewer.yaw.toFixed(2)+separator;
+	link += viewer.pitch.toFixed(2)+separator;
+	link += viewer.x.toFixed(2)+separator;
+	link += viewer.y.toFixed(2)+separator;
+	link += viewer.lastX.toFixed(2)+separator;
+	link += viewer.lastY.toFixed(2);
+	
+	window.location.hash = link;
+}
+
+function ParseAddress()
+{
+	var hash = window.location.hash;
+	hash = hash.replace("#","");
+	
+	var params = hash.split(separator);
+	if(params.length != paramCount)
+		return;
+		
+	viewer.scale = parseFloat(params[0]);
+	viewer.yaw = parseFloat(params[1]);
+	viewer.pitch = parseFloat(params[2]);
+	viewer.x = parseFloat(params[3]);
+	viewer.y = parseFloat(params[4]);
+	viewer.lastX = parseFloat(params[5]);
+	viewer.lastY = parseFloat(params[6]);
+	
+	$("#testConsole").html(viewer.ToString());
+}
+
+function TOP(viewer)
 {
 	viewer.scale = 0.25;
 	viewer.yaw = 0;

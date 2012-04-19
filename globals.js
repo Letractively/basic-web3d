596c96eef25068a4da0f4fb4def9858d07136f43
mksiazek <mksiazek@gmail.com>
2012-04-19 08:35:46 +0000

    Statistics added

--- globals.js
+++ globals.js
@@ -3,12 +3,19 @@
 //--------------------SCENE VARIABLES---------------------//
 var canvas;
 var renderedFrames;
-var scene;
-var root;
+var scene;					//Scene handler
+var root;					//The root node of scene
 var viewer;
 
-var renderOnRequest = true;		//włącza / wyłącza tryb renderingu na żądanie
-var renderRequest = true;		//flaga mówiąca czy należy ponownie wyrenderować scenę
+var renderOnRequest = true;		//on/off render on request
+var renderRequest = true;		//true if new frame have to be rendered (something changed in the scene)
 
-//--------------------EVENTS VARIABLES---------------------//
-//var mouseMoved = false;
+//--------------------STATISTICS VARIABLES---------------------//
+var lastFrameRenderTime;
+var verticesCount;
+var trianglesCount;
+
+//--------------------UI ELEMENTS CONSTANTS---------------------//
+var div_time = "time";
+var div_verticesCount = "verts";
+var div_trianglesCount = "triangles";
